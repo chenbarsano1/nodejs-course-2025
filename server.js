d63b294029e850406ce919ff903c7c3017a39988
Chen Barsano <chenbarsano@gmail.com>
2025-01-26 16:27:52 +0200

    Building a simple API

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 import http from 'http'
-
+import fs from 'fs/promises'
+import url from 'url'
+import path from 'path'
 const PORT = process.env.PORT
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 
 // req - http.IncomingMessage
 // res - http.ServerResponse
-const server = http.createServer((req, res) => {
-    console.log(req.url)
-    console.log(req.method)
-    // res.write('hi')
-    // res.setHeader('Content-Type', 'text/plain')
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.end('<h1>Hello</h1>')
+const server = http.createServer(async (req, res) => {
+    try {
+        if (req.method === 'GET') {
+            let filePath
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html')
+            } else if (req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html')
+            } else {
+                throw new Error('Not Found')
+            }
+            const data = await fs.readFile(filePath)
+            res.setHeader('Content-Type', 'text/html')
+            res.write(data)
+            res.end()
+        } else {
+            throw new Error('Method not allowed')
+        }
+    } catch (error) {
+        res.writeHead(404, {'Content-Type':'text/plain'})
+        res.end('Server Error')
+    }
+    
 })
 
 server.listen(PORT, () => {
