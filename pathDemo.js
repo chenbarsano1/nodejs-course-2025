bf1d579a43082743b67806fcfd821d719837d1e8
Chen Barsano <chenbarsano@gmail.com>
2025-01-26 19:11:15 +0200

    Path module

---
+++ pathDemo.js
@@ -0,0 +1,15 @@
+import path, { basename } from 'path'
+
+const filePath = './dir1/dir2/test.txt'
+
+// basename()
+console.log(path.basename(filePath))
+
+// dirname()
+console.log(path.dirname(filePath))
+
+// extname()
+console.log(path.extname(filePath))
+
+// parse()
+console.log(path.parse(filePath))
